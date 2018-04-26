3d65ce3da3d72054a2d83c3423b713fbc7cc7f63
Ezerous <ezerous@gmail.com>
2018-04-26 22:38:24 +0300

    Travis init

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,12 +1,15 @@
 const login = require('./../lib/auth').login;
+const getCredentials = require('./utils').getCredentials;
 
-it('non default credentials found in config file', async () => {
-    const defaultCredentials = ['username','password'];
-    expect([global.USERNAME, global.PASSWORD]).not.toEqual(defaultCredentials);
+const credentials = getCredentials();
+
+it('non default credentials found in config file or in the environment', async () => {
+    const defaultCredentials = {username: 'username', password: 'password'};
+    expect(credentials).not.toEqual(defaultCredentials);
 });
 
 it('provided valid credentials, login is successful', async () => {
     expect.assertions(1);
-    const loginResult = await login(global.USERNAME, global.PASSWORD);
+    const loginResult = await login(credentials.username, credentials.password);
     expect(JSON.stringify(loginResult)).toEqual(expect.stringContaining("THMMYgrC00ki3"));
 });
