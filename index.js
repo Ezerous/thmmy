aa34046e1d9206527d4dbddb2083eef3cac19ff9
Ezerous <ezerous@gmail.com>
2019-02-27 18:24:41 +0200

    Added option to mark topic as unread

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ exports.login = require('./lib/auth').login;
 exports.getUnreadPosts = require('./lib/unread').getUnreadPosts;
 exports.getRecentPosts = require('./lib/recent').getRecentPosts;
 exports.getTopicBoards = require('./lib/topic').getTopicBoards;
+exports.markTopicAsUnread = require('./lib/topic').markTopicAsUnread;
