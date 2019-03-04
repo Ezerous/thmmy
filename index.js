e04398a439e904f4aaffc364f6142a46cd210004
Ezerous <ezerous@gmail.com>
2019-03-04 12:49:15 +0200

    Added sesc as option to markTopicAsUnread

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 exports.login = require('./lib/auth').login;
+exports.getSesc = require('./lib/auth').getSesc;
 exports.getUnreadPosts = require('./lib/unread').getUnreadPosts;
 exports.getRecentPosts = require('./lib/recent').getRecentPosts;
 exports.getTopicBoards = require('./lib/topic').getTopicBoards;
